7b36fc32068fbd3d32c95dabd2ca4efcd5d67766
Aaron Kamoske <akamoske@gmail.com>
2019-07-01 10:32:52 -0400

    new function

--- Processing_Example.R
+++ Processing_Example.R
@@ -5,7 +5,7 @@ install_github("akamoske/hypRspec")
 library(hypRspec)
 
 # Calculate the NDVI mask
-ndvi <- ndvi.mask(hy.file = "D:/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
+ndvi <- ndvi.mask(hy.file = "D:/Tests/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
                   metadata.path = "/TALL/Reflectance/Reflectance_Data",
                   reflectance.path = "/TALL/Reflectance/Reflectance_Data",
                   wavelength.path = "/TALL/Reflectance/Metadata/Spectral_Data/Wavelength",
@@ -14,13 +14,13 @@ ndvi <- ndvi.mask(hy.file = "D:/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_2018042
                   ndvi.threshold = 0.5)
 
 # Calculate the brightness mask
-brightness <- brightness.mask(hy.file = "D:/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
+brightness <- brightness.mask(hy.file = "D:/Tests/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
                               metadata.path = "/TALL/Reflectance/Reflectance_Data",
                               reflectance.path = "/TALL/Reflectance/Reflectance_Data",
                               wavelength.path = "/TALL/Reflectance/Metadata/Spectral_Data/Wavelength")
 
 # Apply the topographic correction
-hsi.raster <- hsi.correction(hy.file = "D:/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
+hsi.raster <- hsi.correction(hy.file = "D:/Tests/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
                              ndvi.mask = ndvi,
                              brightness.mask = brightness,
                              band.combo = c(25:194, 215:284, 325:403),
@@ -37,6 +37,26 @@ hsi.raster <- hsi.correction(hy.file = "D:/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_
                              ross = "thick",
                              li = "dense",
                              raster.res = 10)
+
+# Apply the topographic correction
+hsi.refl <- hsi.extract(hy.file = "D:/Tests/BRDF_TESTING/TALL_HDF5/NEON_D08_TALL_DP1_20180429_190316_reflectance.h5",
+                        ndvi.mask = ndvi,
+                        brightness.mask = brightness,
+                        band.combo = c(25:194, 215:284, 325:403),
+                        metadata.path = "/TALL/Reflectance/Reflectance_Data",
+                        reflectance.path = "/TALL/Reflectance/Reflectance_Data",
+                        wavelength.path = "/TALL/Reflectance/Metadata/Spectral_Data/Wavelength",
+                        solar.az.path = "/TALL/Reflectance/Metadata/Logs/Solar_Azimuth_Angle",
+                        solar.zn.path = "/TALL/Reflectance/Metadata/Logs/Solar_Zenith_Angle",
+                        slope.path = "/TALL/Reflectance/Metadata/Ancillary_Imagery/Slope",
+                        aspect.path = "/TALL/Reflectance/Metadata/Ancillary_Imagery/Aspect",
+                        sensor.az.path = "/TALL/Reflectance/Metadata/to-sensor_Azimuth_Angle",
+                        sensor.zn.path = "/TALL/Reflectance/Metadata/to-sensor_Zenith_Angle",
+                        coordinate.path = "/TALL/Reflectance/Metadata/Coordinate_System",
+                        ross = "thick",
+                        li = "dense",
+                        shp.file.loc = "C:/Users/Aaron Kamoske/Dropbox/Dissertation/Data/PROCESSED_DATA/SHP_FILES/FOLIAR_DATA/TALL2018",
+                        shp.file.name = "TALL2018_TOC_FoliarData_20190626")
 
 
 
